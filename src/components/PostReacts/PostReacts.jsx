8393c2ff60652b102491c97619dc3411883990f4
jdtoromuriel <jdtoromu@cesde.net>
2024-10-07 21:46:43 -0500

    Se arreglan el sass deprecado

--- src/components/PostReacts/PostReacts.jsx
+++ src/components/PostReacts/PostReacts.jsx
@@ -1,8 +1,22 @@
 import './PostReacts.scss';
-
+import Comment2 from './../../assets/Comments2.svg'
+import Like2 from './../../assets/Like2.svg'
 const PostReacts = () => {
   return (
-    <div>PostReacts</div>
+   <>
+    <div className="Post-Reacts">
+      <section className="Likes-Comments">
+        <div className="Like-Comment">
+          <img src={Like2} alt="" />
+          <span>Me gusta</span>
+        </div>
+        <div className="Like-Comment">
+          <img src={Comment2} alt="" />
+          <span>Comentar</span>
+        </div>
+      </section>
+    </div>
+   </>
   )
 }
 
